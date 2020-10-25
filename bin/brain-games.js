b02dd75b7afbee5502e9f911d80124c3ba79e2b8
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-10-25 16:30:11 +0300

    add user greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+import greeting from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+
+greeting();
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import promptly from 'promptly';
+
+export default async () => {
+    const name = await promptly.prompt('Name: ');
+    console.log(`Hello, ${name}!`);
+};
