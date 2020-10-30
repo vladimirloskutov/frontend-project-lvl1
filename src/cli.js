0ad7d07b746f4d98a312e2edf398716b8fd9fec3
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-10-30 22:23:02 +0300

    update src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,11 @@
 import promptly from 'promptly';
 
 export default async () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('May I have your name?');
+
   const name = await promptly.prompt('Name: ');
-  console.log(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!\n`);
+
+  return name;
 };
