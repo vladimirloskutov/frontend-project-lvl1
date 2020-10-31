1edf80fa49304da079c97a57b8a93aa814d33ab8
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-11-01 00:10:41 +0300

    add brain-prime game

---
+++ src/games/prime.js
@@ -0,0 +1,30 @@
+import runGame from '../index.js';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const run = async () => {
+  const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+  const getGameData = () => {
+    const question = Math.floor(Math.random() * 10);
+    const correctAnswer = isPrime(question) ? 'yes' : 'no';
+
+    return [question, String(correctAnswer)];
+  };
+
+  return runGame(description, getGameData);
+};
+
+export default run;
