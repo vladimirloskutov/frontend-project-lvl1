bcd19ef7412eea5813c29df786644a5bbcfc9d7e
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-10-31 23:31:45 +0300

    add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import run from '../src/games/progression.js';
+
+run();
---
+++ src/games/progression.js
@@ -0,0 +1,33 @@
+import runGame from '../index.js';
+
+const getProgression = () => {
+  const progressionLength = 10;
+  const progressionStart = Math.floor(Math.random() * 10);
+  const progressionStep = Math.floor(Math.random() * 10 + 1);
+  const progression = [];
+
+  for (let i = 0; i < progressionLength; i += 1) {
+    progression.push(progressionStart + progressionStep * i);
+  }
+
+  return progression;
+};
+
+const run = async () => {
+  const description = 'What number is missing in the progression?';
+
+  const getGameData = () => {
+    const progression = getProgression();
+
+    const secretElementIndex = Math.floor(Math.random() * progression.length);
+    const correctAnswer = progression[secretElementIndex];
+    progression[secretElementIndex] = '..';
+    const question = progression.join(' ');
+
+    return [question, String(correctAnswer)];
+  };
+
+  return runGame(description, getGameData);
+};
+
+export default run;
