4aab39f9ce08360e78c94ccc000880ff74798f10
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-11-02 13:27:09 +0300

    make getProgression deterministic

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,9 +1,7 @@
 import runGame from '../index.js';
 
-const getProgression = () => {
+const getProgression = (progressionStart, progressionStep) => {
   const progressionLength = 10;
-  const progressionStart = Math.floor(Math.random() * 10);
-  const progressionStep = Math.ceil(Math.random() * 10);
   const progression = [];
 
   for (let i = 0; i < progressionLength; i += 1) {
@@ -17,7 +15,9 @@ const description = 'What number is missing in the progression?';
 
 const run = async () => {
   const getGameData = () => {
-    const progression = getProgression();
+    const progressionStart = Math.floor(Math.random() * 10);
+    const progressionStep = Math.ceil(Math.random() * 10);
+    const progression = getProgression(progressionStart, progressionStep);
 
     const secretElementIndex = Math.floor(Math.random() * progression.length);
     const correctAnswer = progression[secretElementIndex];
