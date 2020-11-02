185bc781aefc658098d809e57f52238eab7a0882
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-11-02 17:35:51 +0300

    add export to functions

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
-const getRandomNumber = () => Math.ceil(Math.random() * 10);
+export const getRandomNumber = () => Math.ceil(Math.random() * 10);
 
-const getRandomIndex = (array) => Math.floor(Math.random() * array.length);
+export const getRandomIndex = (array) => Math.floor(Math.random() * array.length);
