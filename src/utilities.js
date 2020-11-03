895a14734e8e1e77de87e0a1b6c1c76e1608c72c
Vladimir Loskutov <vladimirloskutov@icloud.com>
2020-11-03 15:41:18 +0300

    remove default values of arguments from get and rename function

--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,3 @@
-const getRandomNumber = (min = 1, max = 10) => Math.floor(Math.random() * max + min - min);
+const getRandomFromRange = (min, max) => Math.floor(Math.random() * max + min - min);
 
-export default getRandomNumber;
+export default getRandomFromRange;
